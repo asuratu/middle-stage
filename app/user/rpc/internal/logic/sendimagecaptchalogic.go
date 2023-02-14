7d8a48c74890ddc1eeac983dab7ba09abce31056
AsuraTu <tugecn@gmail.com>
2023-02-14 18:30:35 +0800

    update

--- app/user/rpc/internal/logic/sendimagecaptchalogic.go
+++ app/user/rpc/internal/logic/sendimagecaptchalogic.go
@@ -27,10 +27,12 @@ func NewSendImageCaptchaLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 func (l *SendImageCaptchaLogic) SendImageCaptcha(in *user.SendImageCaptchaReq) (*user.SendImageCaptchaResp, error) {
 	// 生成验证码
 	id, b64s, err := captcha.NewCaptcha(l.svcCtx.Config, l.svcCtx.Redis).GenerateCaptcha()
+
 	// 记录错误日志，因为验证码是用户的入口，出错时应该记 error 等级的日志
 	if err != nil {
 		logx.Error(err)
 	}
+
 	// 返回给用户
 	return &user.SendImageCaptchaResp{
 		CaptchaId:    id,
