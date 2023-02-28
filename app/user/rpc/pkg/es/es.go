7e0c0c86ebd8d788c3252cb2a638c3e83613085f
AsuraTu <tugecn@gmail.com>
2023-02-28 16:17:11 +0800

    elastic

--- app/user/rpc/pkg/es/es.go
+++ app/user/rpc/pkg/es/es.go
@@ -2,9 +2,13 @@ package es
 
 import (
 	"bytes"
+	"encoding/json"
+	"io"
 	"log"
 	"middle/app/user/rpc/internal/config"
 	"strings"
+
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 
 	"github.com/zeromicro/go-zero/core/logx"
 
@@ -45,13 +49,18 @@ func NewEsClient(c config.Config) (es *Es) {
 
 // IndexExists 检查索引是否存在
 func (es *Es) IndexExists(indexName string) bool {
-	exists, err := es.Client.Indices.Exists([]string{indexName})
+	response, err := es.Client.Indices.Exists([]string{indexName})
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(response.Body)
+
 	if err != nil {
 		log.Fatalf("es index exists error: %s", err)
 	}
-	if exists.StatusCode != 200 {
-		return false
-	}
+
+	isError(response)
+
 	return true
 }
 
@@ -68,13 +77,15 @@ func (es *Es) CreateIndex(indexName, indexBody string) {
 	indexBody = UserMappings
 	response, err := es.Client.Indices.Create(indexName, es.Client.Indices.Create.WithBody(strings.NewReader(UserMappings)))
 
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(response.Body)
+
 	if err != nil {
 		log.Fatalf("es create index error: %s", err)
 	}
 
-	if response.StatusCode != 200 {
-		log.Fatalf("es create index %s error: %v", indexName, response)
-	}
+	isError(response)
 }
 
 // DeleteIndex 删除索引
@@ -84,23 +95,47 @@ func (es *Es) DeleteIndex(indexName string) {
 		return
 	}
 	response, err := es.Client.Indices.Delete([]string{indexName})
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(response.Body)
+
 	if err != nil {
 		log.Fatalf("es delete index %s error: %v", indexName, err)
 	}
-	if response.StatusCode != 200 {
-		log.Fatalf("es delete index %s error: %v", indexName, response)
-	}
+
+	isError(response)
 }
 
 // Insert 索引一条数据
 func (es *Es) Insert(indexName, id string, body *bytes.Buffer) error {
 	response, err := es.Client.Create(indexName, id, body)
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(response.Body)
+
 	if err != nil {
 		logx.Errorf("es index: %s id: %s body %v insert error: %v", indexName, id, body, err)
 		return err
 	}
-	if response.StatusCode != 201 {
-		logx.Errorf("es index: %s id: %s body %v insert error: %v", indexName, id, body, response)
-	}
+
+	isError(response)
+
 	return nil
+}
+
+func isError(response *esapi.Response) {
+	if response.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(response.Body).Decode(&e); err != nil {
+			log.Fatalf("Error parsing the response body: %s", err)
+		} else {
+			log.Fatalf("[%s] %s: %s",
+				response.Status(),
+				e["error"].(map[string]interface{})["type"],
+				e["error"].(map[string]interface{})["reason"],
+			)
+		}
+	}
 }
