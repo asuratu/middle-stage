2c0e675118af33db97c8c957c0c60415fc3004ac
AsuraTu <tugecn@gmail.com>
2023-03-01 18:20:49 +0800

    add start.sh

--- app/user/rpc/usersvc.go
+++ app/user/rpc/usersvc.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"middle/common/interceptor/rpcserver"
 
-	"github.com/zeromicro/go-zero/core/logx"
-
 	"middle/app/user/rpc/internal/config"
 	"middle/app/user/rpc/internal/server"
 	"middle/app/user/rpc/internal/svc"
@@ -36,7 +34,7 @@ func main() {
 		}
 	})
 
-	logx.DisableStat()
+	//logx.DisableStat()
 
 	//rpc log
 	s.AddUnaryInterceptors(rpcserver.LoggerInterceptor)
